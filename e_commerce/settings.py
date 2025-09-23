14a6e13d5c8ac869c1096ad4e423aa49551d9a2e
Namisi-Aaron <aaronnamisi@gmail.com>
2025-09-23 13:47:08 +0300

    fix: django.contrib.sessions.middleware.SessionMiddleware inserted before django.contrib.auth.middleware.AuthenticationMiddleware

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -59,9 +59,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
